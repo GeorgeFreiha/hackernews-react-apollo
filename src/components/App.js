e8adacc61071b01f592cabd30b211bdd99c60003
GeorgeFreiha <george19110@gmail.com>
2022-08-08 15:56:08 +0300

    add routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import '../styles/App.css';
 import CreateLink from './CreateLink';
 import LinkList from './LinkList';
+import Header from './Header';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-   <CreateLink></CreateLink>
+    <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Routes>
+        <Route path="/" element={<LinkList/>} />
+        <Route
+          path="/create"
+          element={<CreateLink/>}
+        />
+      </Routes>
+    </div>
+  </div>
   );
 }
 
